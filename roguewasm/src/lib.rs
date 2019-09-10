8eb3da0abd57f6c78d6c6ce202125f01e653b737
Tim Spence <timothywspence@gmail.com>
2019-09-10 15:33:34 +0100

    Work on roguewasm

--- roguewasm/src/lib.rs
+++ roguewasm/src/lib.rs
@@ -1,12 +1,59 @@
+#[macro_use]
+extern crate serde_derive;
+
 extern crate wasm_bindgen;
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+
+    fn alert(&str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen(module = "./index")]
+    fn stats_updated(stats: JsValue);
+
+    pub type Display;
+
+    #[wasm_bindgen(method, structural, js_namespace = ROT)]
+    fn draw(this: &Display, x: i32, y: i32, ch: &str);
+
+    #[wasm_bindgen(method, structural, js_name = draw, js_namespace = ROT)]
+    fn draw_color(this: &Display, x: i32, y: i32, ch: &str, color: &str);
+
 }
 
 #[wasm_bindgen]
-pub fn hello(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub struct Engine {
+    display: Display,
+    points: HashMap<GridPoint, String>,
+    prize_location: Option<GridPoint>
+}
+
+#[wasm_bindgen]
+impl Engine {
+    #[wasm_bindgen(constructor)]
+    pub fn new(display: Display) -> Engine {
+        Engine {
+            display,
+            points: HashMap::new(),
+            prize_location: None
+        }
+    }
+
+    pub fn on_dig(&mut self, x: i32, y: i32, val: i32) {
+        if val == 0 {
+            let pt = GridPoint { x, y};
+            self.points.insert(pt, ".".to_owned());
+        }
+    }
+
+    pub fn draw_map(&self) {
+        for (k, v) in &self.points {
+            self.display.draw(k.x, k.y, &v);
+        }
+    }
 }
